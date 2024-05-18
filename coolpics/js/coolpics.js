7ddeae1373147fd05c6f092c0f31025af79c8c6c
Nicki030 <nicolaidaj@byui.edu>
2024-05-18 12:51:43 -0500

    COMMIT

--- coolpics/js/coolpics.js
+++ coolpics/js/coolpics.js
@@ -1,25 +1,53 @@
-function viewerTemplate(pic, alt) {
-    return `<div class="viewer">
-      <button class="close-viewer">X</button>
-      <img src="${pic}" alt="${alt}">
-    </div>`;
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector(".menu-button");
+    const menu = document.querySelector(".menu");
 
-function viewHandler(event) {
-    if (event.target.tagName === "IMG") {
-        const pic = event.target.src.replace('-sm', '-full'); 
-        const alt = event.target.alt;
-        const viewerHTML = viewerTemplate(pic, alt);
-        document.body.insertAdjacentHTML("afterbegin", viewerHTML);
-        const closeButton = document.querySelector(".close-viewer");
-        closeButton.addEventListener("click", closeViewer);
+    function toggleMenu() {
+        menu.classList.toggle("hide");
     }
-}
 
-function closeViewer() {
-    const viewer = document.querySelector(".viewer");
-    viewer.parentNode.removeChild(viewer);
-}
+    menuButton.addEventListener("click", toggleMenu);
 
-const gallery = document.querySelector(".gallery");
-gallery.addEventListener("click", viewHandler);
+    function handleResize() {
+        if (window.innerWidth > 1000) {
+            menu.classList.remove("hide");
+        } else {
+            menu.classList.add("hide");
+        }
+    }
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    function viewerTemplate(pic, alt) {
+        return `<div class="viewer">
+          <button class="close-viewer">X</button>
+          <img src="${pic}" alt="${alt}">
+          </div>`;
+    }
+
+    function viewHandler(event) {
+        const target = event.target;
+        if (target.tagName === 'IMG') {
+            const src = target.getAttribute('src');
+            const parts = src.split("-");
+            const fullSrc = `${parts[0]}-full.jpeg`;
+
+            const viewerHTML = viewerTemplate(fullSrc, target.alt);
+            document.body.insertAdjacentHTML("afterbegin", viewerHTML);
+
+            const closeButton = document.querySelector(".close-viewer");
+            closeButton.addEventListener("click", closeViewer);
+        }
+    }
+
+    function closeViewer() {
+        const viewer = document.querySelector(".viewer");
+        if (viewer) {
+            viewer.remove();
+        }
+    }
+
+    const gallery = document.querySelector(".gallery");
+    gallery.addEventListener("click", viewHandler);
+});
