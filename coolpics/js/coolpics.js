a0ab0c81aa725920289df8533aebd102e8907ccf
Nicki030 <nicolaidaj@byui.edu>
2024-05-18 12:06:41 -0500

    commit

--- coolpics/js/coolpics.js
+++ coolpics/js/coolpics.js
@@ -3,23 +3,23 @@ function viewerTemplate(pic, alt) {
       <button class="close-viewer">X</button>
       <img src="${pic}" alt="${alt}">
     </div>`;
-  }
-  
-  function viewHandler(event) {
+}
+
+function viewHandler(event) {
     if (event.target.tagName === "IMG") {
-      const pic = event.target.src.split("-")[0] + "-full.jpeg";
-      const alt = event.target.alt;
-      const viewerHTML = viewerTemplate(pic, alt);
-      document.body.insertAdjacentHTML("afterbegin", viewerHTML);
-      const closeButton = document.querySelector(".close-viewer");
-      closeButton.addEventListener("click", closeViewer);
+        const pic = event.target.src.split("-")[0] + "-full.jpeg";
+        const alt = event.target.alt;
+        const viewerHTML = viewerTemplate(pic, alt);
+        document.body.insertAdjacentHTML("afterbegin", viewerHTML);
+        const closeButton = document.querySelector(".close-viewer");
+        closeButton.addEventListener("click", closeViewer);
     }
-  }
-  
-  function closeViewer() {
+}
+
+function closeViewer() {
     const viewer = document.querySelector(".viewer");
     viewer.parentNode.removeChild(viewer);
-  }
-  
-  const gallery = document.querySelector(".gallery");
-  gallery.addEventListener("click", viewHandler);
+}
+
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", viewHandler);
